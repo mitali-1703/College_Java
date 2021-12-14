e5323ce8266e5aad92b177fc4e450bc18043e89e
Mitali Agrawal <mitaliagrawal4321@gmail.com>
2021-12-15 00:19:02 +0530

    Added basic exception handling programs

--- P17.java
+++ P17.java
@@ -4,6 +4,24 @@ package com.company;
 
 public class P17 {
     public static void main(String[] args) {
+        try{
+            int a = 43/0;
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException occurs");
+        }
 
+        try{
+            String s = null;
+            System.out.println(s.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException occurs");
+        }
+
+        try{
+            int[] arr = {4,0,1,3};
+            System.out.println(arr[4]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException occurs");
+        }
     }
 }
--- P18.java
+++ P18.java
@@ -2,8 +2,30 @@
 
 package com.company;
 
+import java.util.Scanner;
+
 public class P18 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        int sum = a+b;
+        try{
+            if (sum>99)
+                throw new ArithmeticException("Sum is greater than 99");
+            else
+                System.out.println("Sum is: "+sum);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException occurs");
+        }
     }
 }
+
+//OUTPUT
+//Enter first number:
+//100
+//Enter second number:
+//0
+//ArithmeticException occurs
